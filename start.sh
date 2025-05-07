487ca02d14e73b8c2e89a46de10974bbf2b0f129
peixiangluo <peixiangluo@tencent.com>
2025-05-07 19:00:37 +0800

    update

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # configs
-AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003
+# AUUID=$AUUID
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
+
+PREFIX=https://raw.githubusercontent.com/luopeixiang/Railway-Xray/refs/heads/main/etc
+CONFIGCADDY=${PREFIX}/Caddyfile
+CONFIGXRAY=${PREFIX}/xray.json
+StoreFiles=${PREFIX}/StoreFiles
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
