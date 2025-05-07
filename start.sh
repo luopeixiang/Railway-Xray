be64107189d2a88c2666d047c1858c006f8e85c9
peixiangluo <peixiangluo@tencent.com>
2025-05-07 19:12:17 +0800

    update port to 4433

--- start.sh
+++ start.sh
@@ -9,7 +9,8 @@ CONFIGCADDY=${PREFIX}/Caddyfile
 CONFIGXRAY=${PREFIX}/xray.json
 StoreFiles=${PREFIX}/StoreFiles
 ParameterSSENCYPT=chacha20-ietf-poly1305
-#PORT=4433
+
+PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
